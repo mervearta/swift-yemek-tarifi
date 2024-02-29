fc1ea318520208682f0fb6846ceaf4a2f3d09730
Merve Arta <merve.arta@gmail.com>
2024-02-29 16:31:25 +0300

    first commit

---
+++ Yemek Tarifi Uygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yemek Tarifi Uygulamasi
+//
+//  Created by Merve Arta on 21.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
